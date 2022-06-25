d4ce30d66790d8788f13adfbe2e3ea3d6e02daa8
Sejung Kwak <kwak.sejung@gmail.com>
2022-06-25 23:35:23 +0000

    Simple Filtering, Complex Filtering

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === 'Paul');
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,14 @@ const students = [
     ]
   },
 ];
+
+// const candidates = students.filter(student => {
+//     let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5)
+//     return strongSkills.length > 0;
+// })
+
+const has5yrsExp = skill => skill.yrsExperience >= 5
+const hasStrongSkills = student => student.skills.filter(has5yrsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+const names = candidates.map(candidate => candidate.name)
+console.log(names);
